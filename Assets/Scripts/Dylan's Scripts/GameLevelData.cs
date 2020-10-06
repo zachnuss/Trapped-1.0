9b3b64876c9bbbc5e168afe57d7b8787bc2bcdc0
Dylan Loe <dylanloe@gmail.com>
2020-10-06 16:49:35 -0700

    intial setup for permutation setup

--- Assets/Scripts/Dylan's Scripts/GameLevelData.cs
+++ Assets/Scripts/Dylan's Scripts/GameLevelData.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 /// Keeps track of permutations and chooses which level to go when
 /// - each scene has the permutation already in the, player data calls to this obj to choose which one we use and which ones we are not
 /// 
+/// NOTE: future iteration will incorperate a cube class that has a type, on construciton it will pick 6 random sides from its corresponding permutation and place them accordingly
 /// 
 /// PlayerData takes from here to choose the next level
 /// </summary>
 [CreateAssetMenu(fileName = "GameLevelData", menuName = "ScritableObjects/GameLevelData", order = 2)]
 public class GameLevelData : ScriptableObject
 {
+    //future iteration will have these be walls instead of entire cubes
     [Header("Level 1 Permutations")]
     public GameObject[] level1Permutations;
     [Header("Level 2 Permutations")]
@@ -26,15 +28,19 @@ public class GameLevelData : ScriptableObject
         switch (levelType)
         {
             case levelTypeE.EasyLevel:
-                permutation = Random.Range(0, level1Permutations.Length);
+                permutation = Random.Range(0, level1Permutations.Length - 1);
+                Debug.Log("Used p number: " + permutation);
                 return level1Permutations[permutation];
             case levelTypeE.MidLevel:
-                permutation = Random.Range(0, level2Permutations.Length);
+                permutation = Random.Range(0, level2Permutations.Length - 1);
+                Debug.Log("Used p number: " + permutation);
                 return level2Permutations[permutation];
             case levelTypeE.Hardlevel:
-                permutation = Random.Range(0, level3Permutations.Length);
+                permutation = Random.Range(0, level3Permutations.Length - 1);
+                Debug.Log("Used p number: " + permutation);
                 return level3Permutations[permutation];
             default:
+                Debug.Log("Null Permutation");
                 return null;
         }
     }
--- Assets/Scripts/Dylan's Scripts/LevelSetup.cs
+++ Assets/Scripts/Dylan's Scripts/LevelSetup.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+/// <summary>
+/// Future iteration will use a cube class to create the sides and set that up instead of chooseing what cube we use we choose what sides on said cube
+/// </summary>
 public enum levelTypeE {
     EasyLevel,
     MidLevel,
@@ -11,12 +13,37 @@ public enum levelTypeE {
 
 public class LevelSetup : MonoBehaviour
 {
-    GameLevelData gameLevelData;
+    public GameLevelData gameLevelData;
 
     public levelTypeE type;
 
+    public GameObject permutation;
+
+    private GameObject _player;
+
     private void Awake()
     {
-        
+        _player = GameObject.FindGameObjectWithTag("Player");
+        SetPlayer();
+        permutation = gameLevelData.ChooseLevelP(type);
+        Instantiate(permutation);
+    }
+
+    void SetPlayer()
+    {
+        switch (type)
+        {
+            case levelTypeE.EasyLevel:
+                _player.transform.position = new Vector3(0.45f, 23.01f, 0.27f);
+                break;
+            case levelTypeE.MidLevel:
+                _player.transform.position = new Vector3(0, 2308.998f, 0);
+                break;
+            case levelTypeE.Hardlevel:
+                _player.transform.position = new Vector3(0, 38.1f, 0);
+                break;
+            default:
+                break;
+        }
     }
 }

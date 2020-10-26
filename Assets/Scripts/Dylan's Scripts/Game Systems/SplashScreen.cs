33c7bde2901c5d4d23f6e521337fa424083a5ca4
Dylan Loe <dylanloe@gmail.com>
2020-10-26 15:40:14 -0700

    added splash screen

---
+++ Assets/Scripts/Dylan's Scripts/Game Systems/SplashScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour
+{
+    /// <summary>
+    /// Goes to this scene first
+    /// 
+    /// prompts player to plug in controller and press any button
+    /// (then takes them to main menu)
+    /// </summary>
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Shawn_MainMenu");
+    }
+}

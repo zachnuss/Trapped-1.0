84cf9fb9d70597596a089ec9863971980c33886f
Dylan Loe <dylanloe@gmail.com>
2020-11-12 16:31:13 -0800

    fixed bug with player reference to collider

--- Assets/Scripts/Dylan's Scripts/Player Behaviors/PlayerMovement.cs
+++ Assets/Scripts/Dylan's Scripts/Player Behaviors/PlayerMovement.cs
@@ -323,7 +323,7 @@ public class PlayerMovement : MonoBehaviour
 
         transform.localPosition = Vector3.zero;
         //this.GetComponent<Rigidbody>().isKinematic = true;
-        this.GetComponent<SphereCollider>().isTrigger = true;
+        this.GetComponent<Collider>().isTrigger = true;
 
         follower.transform.parent = PlayerRotate.transform;
         //Debug.Log(follower.transform.localEulerAngles);

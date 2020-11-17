f50813f450058cbfddba84976e7d058c8488b515
firetiger5 <70830300+firetiger5@users.noreply.github.com>
2020-11-17 14:46:38 -0800

    Finished the Achivement Checker
    Finshed the Achievements Scene
    
    Gave a quick update to the Character Customization scene for Wesley

--- Assets/Scripts/Alex's Scripts/UI/UIAchievements.cs
+++ Assets/Scripts/Alex's Scripts/UI/UIAchievements.cs
@@ -16,25 +16,104 @@ public class UIAchievements : MonoBehaviour
     //Private text variable linked to the achievement descriptions for the game objects
     public Text description;
 
-    //Start function is called before the first frame update.
+
+    /// <summary>
+    /// Alexander
+    /// Updated: 11-17-2020
+    /// 
+    /// Start function to disable the complete text at the beginning of the scene
+    /// </summary>
     private void Start()
     {
         achivementComplete.enabled = false;
     }
 
-    //Function for when the Controller overs over a button
+
+    /// <summary>
+    /// Alexander
+    /// Updated: 11-17-2020
+    /// 
+    /// Function to go through all the achievemnt checker functions made by Wesley in Player Data in a bool and if true is returned then enable the complete text.
+    /// </summary>
+    private bool CheckAchievement()
+    {
+        bool result = false; //Variable to keep track of the bool
+
+        //**Tester**
+        Debug.Log("This is the achievment: " + gameObject.name);
+
+        //Set of if-else statements to track the achievements
+        if (gameObject.name == "First Timer")
+        {
+            result = playerdata.achievementFirstTimer;
+        }
+        else if (gameObject.name == "We got a Runner!")
+        {
+            result = playerdata.achievementRunner;
+        }
+        else if (gameObject.name == "Vaccum Murderer")
+        {
+            result = playerdata.achievementVacuumMurderer;
+        }
+        else if (gameObject.name == "Frequent Jail Bird")
+        {
+            result = playerdata.achievementJailBird;
+        }
+        else if (gameObject.name == "Vengence 1")
+        {
+            result = playerdata.achievementRevenge1;
+        }
+        else if (gameObject.name == "Vengence 2")
+        {
+            result = playerdata.achievementRevenge2;
+        }
+        else if (gameObject.name == "Does it End?")
+        {
+            result = playerdata.achievementDoesItEnd;
+        }
+        else if (gameObject.name == "Don't Trust Anyone")
+        { 
+            result = playerdata.achievementNoTrust;
+        }
+        else if (gameObject.name == "Full Prison Wallet")
+        {
+            result = playerdata.achievementFullWallet;
+        }
+        else if (gameObject.name == "Red Means Dead")
+        {
+            result = playerdata.achievementRedDead;
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Alexander
+    /// Updated: 11-17-2020
+    /// 
+    /// Function for when the Controller hovers over a button and checks to see if the achievement was completed
+    /// </summary>
     public void Selected()
     {
         achievementTitle.text = gameObject.name;
         achievementDescription.text = description.text;
-        //achivementComplete.enabled = true; //AHL : TEST!
-        Debug.Log("Controller is over " + gameObject.name + ".");
+        //Check to see if the achievement was completed
+        if(CheckAchievement() == true)
+        {
+            achivementComplete.enabled = true;
+        }
     }
 
-    //Function for when the Controller leaves a button
+    /// <summary>
+    /// Alexander
+    /// Updated: 11-17-2020
+    /// 
+    /// Function for when the Controller leaves a button and disables the complete text if it was active
+    /// </summary>
     public void Deselected()
     {
-        //achivementComplete.enabled = false; //AHL : TEST!
-        Debug.Log("Controller is no longer over " + gameObject.name + ".");
+        //If the achivement completed text was active then disable it
+        if(achivementComplete.enabled == true)
+            achivementComplete.enabled = false;
     }
 }

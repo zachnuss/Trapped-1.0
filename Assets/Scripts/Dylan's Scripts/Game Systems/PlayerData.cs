0b026fe38b4a0e02a6db8355badfacf2931a24ef
WeaselB64 <morrisonwesley@hotmail.com>
2020-11-16 16:29:04 -0800

    Readding petchoice
    Original lines got removed during an update, and I missed it.

--- Assets/Scripts/Dylan's Scripts/Game Systems/PlayerData.cs
+++ Assets/Scripts/Dylan's Scripts/Game Systems/PlayerData.cs
@@ -695,19 +695,22 @@ public class PlayerData : ScriptableObject
     /// </summary>
     public void SetPet()
     {
+        GameObject pet1 = GameObject.Find("pet_wasp");
+        GameObject pet2 = GameObject.Find("Pet2");
         if(petChoice == 0)
         {
-            //disable both pet models
+            pet1.GetComponent<Renderer>().enabled = false;
+            pet2.GetComponent<Renderer>().enabled = false;
         }
         if(petChoice == 1)
         {
-            //disable pet model 2
-            //enable pet model 1
+            pet2.GetComponent<Renderer>().enabled = false;
+            pet1.GetComponent<Renderer>().enabled = true;
         }
         if(petChoice == 2)
         {
-            //disable pet model 1
-            //endable pet model 2
+            pet1.GetComponent<Renderer>().enabled = false;
+            pet2.GetComponent<Renderer>().enabled = true;
         }
     }
 

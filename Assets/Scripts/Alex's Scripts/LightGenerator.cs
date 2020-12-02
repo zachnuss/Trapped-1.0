30d12d56e5f7dfb164b4966bc835289157f77fcf
firetiger5 <70830300+firetiger5@users.noreply.github.com>
2020-12-01 16:52:51 -0800

    Kinda working
    Lights are being generated but not rendered o.o hmmmmmm

--- Assets/Scripts/Alex's Scripts/LightGenerator.cs
+++ Assets/Scripts/Alex's Scripts/LightGenerator.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class LightGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Alexander
+    /// Updated: 12-1-2020
+    /// 
+    /// Variables used to hold the light prefab and in which directions to create the light patterns
+    /// </summary>
+    public GameObject Light;
+    public bool ToTheRight;
+    public int Columns;
+    public int Rows;
+    public float LightCreationPause;
+
+    /// <summary>
+    /// Alexander
+    /// Updated: 12-1-2020
+    /// 
+    /// Happens before the first frame update
+    /// Starts by calling the Coroutine to make the appropriate number of lights going at different times
+    /// </summary>
+    public void Awake()
     {
-        
+        StartCoroutine(createLights(LightCreationPause));
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Alexander
+    /// Updated: 12-1-2020
+    /// 
+    /// Coroutine function to create the lights at varying times
+    /// </summary>
+    IEnumerator createLights(float lightPause)
     {
-        
+        Debug.Log("Starting light cycle");
+
+        //For loop to go through all the rows for the floor pattern
+        for (int row = 0; row < Rows; row++)
+        {
+            Instantiate(Light, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + row), Quaternion.identity);
+
+            Debug.Log("Creating a light");
+
+            //Waits for the certain amount of time before creating the lights
+            yield return new WaitForSeconds(lightPause);
+        }
     }
 }

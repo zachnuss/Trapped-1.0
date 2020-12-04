f3014358e0e8a2f1fc75646a53965e3754198bda
firetiger5 <70830300+firetiger5@users.noreply.github.com>
2020-12-03 16:13:09 -0800

    Lights are working
    Adjustments being made to attach the generator to the level key and get those aspects going

--- Assets/Scripts/Alex's Scripts/LightGenerator.cs
+++ Assets/Scripts/Alex's Scripts/LightGenerator.cs
@@ -6,14 +6,13 @@ public class LightGenerator : MonoBehaviour
 {
     /// <summary>
     /// Alexander
-    /// Updated: 12-1-2020
+    /// Updated: 12-3-2020
     /// 
     /// Variables used to hold the light prefab and in which directions to create the light patterns
     /// </summary>
     public GameObject Light;
-    public bool ToTheRight;
-    public int Columns;
-    public int Rows;
+    public enum Floor { Top, Front, Left, Right, Back, Bottom }; //Sets up an enum to track the side of the cube
+    public Floor face = Floor.Top; //Public switch for the level designers to adjust things
     public float LightCreationPause;
 
     /// <summary>
@@ -30,23 +29,98 @@ public class LightGenerator : MonoBehaviour
 
     /// <summary>
     /// Alexander
-    /// Updated: 12-1-2020
+    /// Updated: 12-3-2020
     /// 
     /// Coroutine function to create the lights at varying times
     /// </summary>
     IEnumerator createLights(float lightPause)
     {
-        Debug.Log("Starting light cycle");
-
-        //For loop to go through all the rows for the floor pattern
-        for (int row = 0; row < Rows; row++)
+        //For loop to go through all the rows for the floor pattern with if loops to distinquish between them
+        
+        if(face == Floor.Top) //Top Section
         {
-            Instantiate(Light, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + row), Quaternion.identity);
+            for (int col = 0; col < 3; col++)
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    Instantiate(Light, new Vector3(this.transform.position.x + col, this.transform.position.y, this.transform.position.z + row), Quaternion.identity);
 
-            Debug.Log("Creating a light");
-
-            //Waits for the certain amount of time before creating the lights
-            yield return new WaitForSeconds(lightPause);
+                    //Waits for the certain amount of time before creating the lights
+                    yield return new WaitForSeconds(lightPause);
+                }
+            }
         }
+
+        if (face == Floor.Front) //Front Section
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    Instantiate(Light, new Vector3(this.transform.position.x + col, this.transform.position.y + row, this.transform.position.z), Quaternion.identity);
+
+                    //Waits for the certain amount of time before creating the lights
+                    yield return new WaitForSeconds(lightPause);
+                }
+            }
+        }
+
+        if (face == Floor.Left) //Left Section
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    Instantiate(Light, new Vector3(this.transform.position.x, this.transform.position.y + row, this.transform.position.z - col), Quaternion.identity);
+
+                    //Waits for the certain amount of time before creating the lights
+                    yield return new WaitForSeconds(lightPause);
+                }
+            }
+        }
+
+        /*if(face == Floor.Right) //Right Section
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    Instantiate(Light, new Vector3(this.transform.position.x + col, this.transform.position.y, this.transform.position.z + row), Quaternion.identity);
+
+                    //Waits for the certain amount of time before creating the lights
+                    yield return new WaitForSeconds(lightPause);
+                }
+            }
+        }
+
+        if(face == Floor.Back) //Back Section
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    Instantiate(Light, new Vector3(this.transform.position.x + col, this.transform.position.y, this.transform.position.z + row), Quaternion.identity);
+
+                    //Waits for the certain amount of time before creating the lights
+                    yield return new WaitForSeconds(lightPause);
+                }
+            }
+        }
+
+        if(face == Floor.Bottom) //Bottom Section
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    Instantiate(Light, new Vector3(this.transform.position.x + col, this.transform.position.y, this.transform.position.z + row), Quaternion.identity);
+
+                    //Waits for the certain amount of time before creating the lights
+                    yield return new WaitForSeconds(lightPause);
+                }
+            }
+        }
+        */
     }
+
 }
